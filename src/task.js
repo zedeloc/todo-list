800f0b6fa44817052f491710c03892d0e64b2212
zedeloc <desmondcole1990@gmail.com>
2025-06-02 01:24:56 -0600

    Include isComplete class property as a constructor option to simplify reloading app from localdata.

--- src/task.js
+++ src/task.js
@@ -1,12 +1,10 @@
 export class Task {
     id = crypto.randomUUID();
-    constructor(taskName, description, priority, notes=undefined) {
+    constructor(taskName, description, priority, notes=undefined, isComplete=false) {
         this.taskName = taskName;
         this.description = description;
         this.priority = priority;
         this.notes = notes;
+        this.isComplete = isComplete
     }
-    isComplete = false;
-
-    // function toggleComplete() ?
 }
